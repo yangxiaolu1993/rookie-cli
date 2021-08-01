0eb8779df841a011c3a061f7de2f4afee94a925b
yangxiaolu3 <yangxiaolu3@jd.com>
2021-08-01 16:32:12 +0800

    更新

--- src/utils/path.ts
+++ src/utils/path.ts
@@ -2,7 +2,7 @@ import { join, resolve } from 'path';
 
 // cli 目录
 export const ROOT_CLI_PATH = function(dir: string) {
-	return resolve(__dirname, '../../', dir);
+	return resolve(__dirname, '../', dir);
 };
 
 
---
+++ src/utils/template.ts
@@ -0,0 +1,5 @@
+
+
+export const template = {
+    normal:"https://github.com:yangxiaolu1993/rookie-cli#rookie-cli-template"
+}
