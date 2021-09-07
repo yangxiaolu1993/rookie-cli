76bdbeff49185bc1530205e50f5932a894f9ee6c
yangxiaolu3 <yangxiaolu3@jd.com>
2021-09-07 09:28:02 +0800

    vite 模板

--- src/router/routes.ts
+++ src/router/routes.ts
@@ -2,6 +2,7 @@ import Home from '@/views/home.vue'
 
 const routes = [
     { path: '/home',name: 'home',component: Home,meta:{title:'首页'} }, // 首页
+    { path: '/',redirect:'/home'}, // 首页
 ]
 
 export default routes
